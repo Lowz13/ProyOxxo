b3e199a2fe9a4f91a73446d866e6b0a751df6f9d
sebasgguwu <122837837+sebasgguwu@users.noreply.github.com>
2024-05-17 09:36:11 -0600

    Interfaz de inicio con boton Distribuidor :)
    Ya sirve el botón de distribuidor xd

--- ProyOxxo/src/main/java/fes/aragon/controller/InicioController.java
+++ ProyOxxo/src/main/java/fes/aragon/controller/InicioController.java
@@ -1,14 +1,27 @@
 package fes.aragon.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class InicioController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button bntDistribuidor;
+
+    @FXML
+    private BorderPane btpPrincipal;
+
+    @FXML
+    void abrirDistribuidor(ActionEvent event) {
+        try {
+            Contenido contenido= new Contenido("/fes/aragon/xml/distribuidor.fxml");
+            btpPrincipal.setCenter(contenido);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- ProyOxxo/src/main/java/fes/aragon/inicio/Inicio.java
+++ ProyOxxo/src/main/java/fes/aragon/inicio/Inicio.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class Inicio extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Inicio.class.getResource("/fes/aragon/xml/inventario.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Inventarios Oxxo");
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setScene(scene);
         stage.show();
     }
