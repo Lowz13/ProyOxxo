b6b6cddeea97477b8256ee72823e4b9e6a1a39e1
Alan <143666779+Alanmilo30@users.noreply.github.com>
2024-05-20 18:35:31 -0600

    Ya con la tabla de producto

---
+++ ProyOxxo 2/src/main/java/fes/aragon/controller/ProductoController.java
@@ -0,0 +1,52 @@
+package fes.aragon.controller;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.input.MouseEvent;
+
+public class ProductoController {
+
+    @FXML
+    private TableColumn<?, ?> cmlCaducidad;
+
+    @FXML
+    private TableColumn<?, ?> cmlCantidad;
+
+    @FXML
+    private TableColumn<?, ?> cmlOperaciones;
+
+    @FXML
+    private TableColumn<?, ?> cmlPrecioUnitario;
+
+    @FXML
+    private TableColumn<?, ?> cmlPrecioVenta;
+
+    @FXML
+    private TableColumn<?, ?> cmlProducto;
+
+    @FXML
+    private FontAwesomeIconView iconCargarProducto;
+
+    @FXML
+    private FontAwesomeIconView iconNuevoProducto;
+
+    @FXML
+    private FontAwesomeIconView iconSalvar;
+
+    @FXML
+    void accionAbrirProducto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void accionNuevoProducto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void accionSalvarProducto(MouseEvent event) {
+
+    }
+
+}
---
+++ ProyOxxo 2/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module fes.aragon.proyoxxo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+    requires de.jensd.fx.glyphs.fontawesome;
+
+    exports fes.aragon.controller;
+    opens fes.aragon.controller to javafx.fxml;
+    exports fes.aragon.inicio;
+    opens fes.aragon.inicio to javafx.fxml;
+}
