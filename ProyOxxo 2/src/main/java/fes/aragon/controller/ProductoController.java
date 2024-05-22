6da323d2e4dc88e9efaa451bdeef07e01c74adb9
Alanmilo30 <alanmilo30@gmail.com>
2024-05-21 22:14:00 -0600

    Modificación 2.1

--- ProyOxxo 2/src/main/java/fes/aragon/controller/ProductoController.java
+++ ProyOxxo 2/src/main/java/fes/aragon/controller/ProductoController.java
@@ -3,9 +3,17 @@ package fes.aragon.controller;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import fes.aragon.modelo.Producto;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class ProductoController {
 
@@ -46,6 +54,20 @@ public class ProductoController {
 
     @FXML
     void accionNuevoProducto(MouseEvent event) {
+        try {
+            Parent parent= FXMLLoader.load(getClass().getResource("/fes/aragon/xml/agregarProducto.fxml"));
+            Scene scene= new Scene(parent);
+            Stage stage=new Stage();
+            stage.setScene(scene);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+
+
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
 
     }
 
