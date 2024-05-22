8ef65f3023a4cdce3ef259f993afee982c73421b
Alanmilo30 <alanmilo30@gmail.com>
2024-05-21 22:08:40 -0600

    Modificación dos

--- ProyOxxo 2/src/main/java/fes/aragon/modelo/Producto.java
+++ ProyOxxo 2/src/main/java/fes/aragon/modelo/Producto.java
@@ -3,74 +3,68 @@ package fes.aragon.modelo;
 import java.io.Serializable;
 
 public class Producto implements Serializable {
-    private String Nombre;
-    private Double Caducidad;
-    private Double Cantidad;
-    private String PrecioUnitario;
-    private String PrecioVenta;
+    private String nombre;
+    private String caducidad;
+    private String cantidad;
+    private String precioUnitario;
+    private String precioVenta;
 
     private Serializable imagen;
 
     public Producto() {
     }
 
-    public Producto(String nombre, Double caducidad, Double cantidad, String precioUnitario, String precioVenta) {
-        Nombre = nombre;
-        Caducidad = caducidad;
-        Cantidad = cantidad;
-        PrecioUnitario = precioUnitario;
-        PrecioVenta = precioVenta;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public void setNombre(String nombre) {
-        Nombre = nombre;
-    }
-
-    public Double getCaducidad() {
-        return Caducidad;
-    }
-
-    public void setCaducidad(Double caducidad) {
-        Caducidad = caducidad;
-    }
-
-    public Double getCantidad() {
-        return Cantidad;
-    }
-
-    public void setCantidad(Double cantidad) {
-        Cantidad = cantidad;
-    }
-
-    public String getPrecioUnitario() {
-        return PrecioUnitario;
-    }
-
-    public void setPrecioUnitario(String precioUnitario) {
-        PrecioUnitario = precioUnitario;
-    }
-
-    public String getPrecioVenta() {
-        return PrecioVenta;
-    }
-
-    public void setPrecioVenta(String precioVenta) {
-        PrecioVenta = precioVenta;
-    }
-    public SerializableImage getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(SerializableImage imagen) {
+    public Producto(String nombre, String caducidad, String cantidad, String precioUnitario, String precioVenta, Serializable imagen) {
+        this.nombre = nombre;
+        this.caducidad = caducidad;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+        this.precioVenta = precioVenta;
         this.imagen = imagen;
     }
 
-    public String toString() {
-        return "Nombre: " + Nombre + ", Caducidad: " + Caducidad + ", Cantidad: " + Cantidad + ", PrecioUnitario" + PrecioUnitario + ", PrecioVenta" + PrecioVenta;
+    public String getNombre() {
+        return nombre;
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCaducidad() {
+        return caducidad;
+    }
+
+    public void setCaducidad(String caducidad) {
+        this.caducidad = caducidad;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(String cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(String precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public String getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(String precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+
+
+
 }
-}
+
