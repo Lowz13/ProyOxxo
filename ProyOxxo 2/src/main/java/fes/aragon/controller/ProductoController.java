a596c0f9fff07a4e972958772bb9e50f95b41615
starboyalan <alanmilo30@gmail.com>
2024-05-21 18:50:44 -0600

    Producto avanzado

--- ProyOxxo 2/src/main/java/fes/aragon/controller/ProductoController.java
+++ ProyOxxo 2/src/main/java/fes/aragon/controller/ProductoController.java
@@ -1,29 +1,34 @@
 package fes.aragon.controller;
 
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import fes.aragon.modelo.Producto;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 
 public class ProductoController {
 
     @FXML
-    private TableColumn<?, ?> cmlCaducidad;
+    private TableColumn<Producto, Double> cmlCaducidad;
 
     @FXML
-    private TableColumn<?, ?> cmlCantidad;
+    private TableColumn<Producto, Double> cmlCantidad;
 
     @FXML
-    private TableColumn<?, ?> cmlOperaciones;
+    private TableColumn<Producto, String> cmlOperaciones;
 
     @FXML
-    private TableColumn<?, ?> cmlPrecioUnitario;
+    private TableColumn<Producto, String> cmlPrecioUnitario;
 
     @FXML
-    private TableColumn<?, ?> cmlPrecioVenta;
+    private TableColumn<Producto, String> cmlPrecioVenta;
 
     @FXML
-    private TableColumn<?, ?> cmlProducto;
+    private TableColumn<Producto, String> cmlNombre;
+
+    @FXML
+    private TableView<Producto> tblTabla;
 
     @FXML
     private FontAwesomeIconView iconCargarProducto;
