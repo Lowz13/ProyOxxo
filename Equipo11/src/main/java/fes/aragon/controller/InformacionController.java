d7670d854bde504f15ce9bb82bc25c66b989fb2b
sebasgguwu <sebaskuhl2003@gmail.com>
2024-05-27 12:24:13 -0600

    informacion xml hecho

--- Equipo11/src/main/java/fes/aragon/controller/InformacionController.java
+++ Equipo11/src/main/java/fes/aragon/controller/InformacionController.java
@@ -1,13 +1,29 @@
 package fes.aragon.controller;
 
+import fes.aragon.modelo.Distribuidor;
 import fes.aragon.modelo.Producto;
 import fes.aragon.modelo.SingeltonProducto;
+import fes.aragon.modelo.SingletonProveedor;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
 public class InformacionController {
     private Integer indice;
+
+    @FXML
+    private ImageView imgInfoProv;
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtDireccion;
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtTelefono;
+
 
     @FXML
     private ImageView imgInfoProd;
@@ -36,6 +52,15 @@ public class InformacionController {
         txtInfoPreV.setText(producto.getPrecioVenta());
         System.out.println(producto.getImagen());
         imgInfoProd.setImage(producto.getImagen().getImage());
+
+        Distribuidor distribuidor = SingletonProveedor.getInstance().getLista().get(indice);
+        txtNombre.setText(distribuidor.getNombre());
+        txtCorreo.setText(distribuidor.getCorreo());
+        txtTelefono.setText(distribuidor.getTelefono());
+        txtDireccion.setText(distribuidor.getDireccion());
+        System.out.println(distribuidor.getImagen());
+        imgInfoProv.setImage(distribuidor.getImagen().getImage());
     }
+
 
 }
